a89056f137bf6c32583ced3f211f7e0087fde48c
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-08-11 09:21:48 -0600

    tests: updated test snapshots

--- test/production/app-dir/metadata-streaming-config/metadata-streaming-config.test.ts
+++ test/production/app-dir/metadata-streaming-config/metadata-streaming-config.test.ts
@@ -40,6 +40,11 @@ describe('app-dir - metadata-streaming-config', () => {
          "type": "header",
          "value": "Mediapartners-Google|Chrome-Lighthouse|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|Yeti",
        },
+       "/_not-found": {
+         "key": "user-agent",
+         "type": "header",
+         "value": "Mediapartners-Google|Chrome-Lighthouse|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|Yeti",
+       },
        "/dynamic": {
          "key": "user-agent",
          "type": "header",
